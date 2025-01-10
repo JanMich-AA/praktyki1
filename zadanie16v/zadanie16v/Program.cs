4b1e3a62411d42c56062b3ebb908cdfc5e1af5ee
JanMich-AA <zsejanmich@interia.pl>
2025-01-10 10:36:05 +0100

    Add files via upload

---
+++ zadanie16v/zadanie16v/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Zadanie16v
+{
+    class Duck : IComparable<Duck>
+    {
+        public int Size;
+        public TypeOfDuck Kind;
+
+        public Duck(int Size, TypeOfDuck Kind)
+        {
+            this.Size = Size;
+            this.Kind = Kind;
+        }
+
+        public Duck(int Size, int Kind)
+        {
+            this.Size = Size;
+            this.Kind = (TypeOfDuck)Kind;
+        }
+
+        public string GetString()
+        {
+            return "A " + Kind.ToString() + " duck that is " + Size + " cm long.";
+        }
+
+        public int CompareTo(Duck duckToCompare)
+        {
+            if (Size > duckToCompare.Size) return 1;
+            else if (Size < duckToCompare.Size) return -1;
+            else return 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck(17, TypeOfDuck.Mallard),
+                new Duck(18, TypeOfDuck.Muscovy),
+                new Duck(14, TypeOfDuck.Decoy),
+                new Duck(11, TypeOfDuck.Muscovy),
+                new Duck(14, TypeOfDuck.Mallard),
+                new Duck(13, TypeOfDuck.Decoy)
+            };
+
+            ducks.Sort();
+
+            foreach (Duck duck in ducks) Console.WriteLine(duck.GetString());
+        }
+    }
+
+    public enum TypeOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Decoy
+    }
+}
